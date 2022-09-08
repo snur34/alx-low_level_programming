a83383f36fcbcfcecc760c2097beae50c8aa39bd
snur34 <a607514@gmail.com>
2022-09-08 15:02:03 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,10 @@
 
 /**
  *
- * main - Entry point
- *
- * 
- *
- * Return: Always 0 (Success)
- *
+ * main - print the string in the put function
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 
 int main(void)
