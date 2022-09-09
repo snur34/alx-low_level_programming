b6e1f63b7ff339288c6c42840ee8999a97f0e5a7
snur34 <a607514@gmail.com>
2022-09-09 15:14:51 -0700

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,6 @@ int main(void)
 	lastd = n % 10;
 	
 	if (lastd > 5)
-
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
@@ -30,4 +29,4 @@ int main(void)
 	}
 
 	return (0);
-}	
+}
