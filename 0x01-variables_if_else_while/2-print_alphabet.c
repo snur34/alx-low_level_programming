10c996c9369fe872fe55cadf06bb810106d11e57
snur34 <a607514@gmail.com>
2022-09-10 11:35:18 -0700

    edit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,19 @@
 #include <stdio.h>
+
 /**
- *main - print numbers from 00 to 99.
+ * main - prints the alphabet in lowercase.
  *
- *Return: 0 on success
+ * Return: 0 on success
  */
 int main(void)
 {
-	int i = '0';
-	int j = '0';
+	char c = 'a';
 
-	while (i <= '9')
+	while (c <= 'z')
 	{
-		while (j <= '9')								{
-			putchar(i);									putchar(j);
-			if (j == '9' && i == '9')
-			{
-				putchar('\n');
-			}
-			else										{
-				putchar(',');									putchar(' ');
-		
-			}
-			j++;
-		}
-		if (j >= '9')
-		{
-			j = '0';								}
-		i++;
+		putchar(c);
+		c++;
 	}
+	putchar('\n');
 	return (0);
 }
