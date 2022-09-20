f726b9bdf8556ee3bccc0eb291ec54905965acaa
snur34 <a607514@gmail.com>
2022-09-20 12:57:22 -0700

    t5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,7 +15,8 @@ void rev_string(char *s)
 
 		for (i = 0; i < fcounter; i++)
 		{
-			fcounter--;									rev = s[i];
+			fcounter--;		
+			rev = s[i];
 			s[i] = s[fcounter];
 			s[fcounter] = rev;
 		}
