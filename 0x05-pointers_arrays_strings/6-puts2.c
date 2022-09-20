e947b872ea91b8c716720e8d33912f4b05b640e9
snur34 <a607514@gmail.com>
2022-09-20 13:42:40 -0700

    t6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,17 @@
 void puts2(char *str)
 {
 		int i;
-		
-		while (str[i] != '\0')
+		int j = 0;
+
+		while (str[j] != '\0')
 		{
-			if (i % 2 == 0)
-				_putchar(str[i]);
-			i++;
+			j++;
 		}
+
+		for (i = 0; i < j; i += 2)
+		{
+			_putchar(str[i]);
+		}
+
 		_putchar('\n');
 }
