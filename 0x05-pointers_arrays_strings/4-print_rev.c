bd24330291c757ccaf9f58cdc79ed2511032051e
snur34 <a607514@gmail.com>
2022-09-20 11:24:02 -0700

    t4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,22 @@
+#include "holberton.h"
 /**
- *print_rev - prints a string in stdout in reverse
- *@s: string to print
- *
- *Return: void
- *
- * /
+ *print_rev - a function that prints a string, in reverse
+ *fcounter is to first count to end, n is to count back
+ *@s: str input
+ *Return: string in reverse
+ */
 void print_rev(char *s)
 {
-		int i = 0;
+		int fcounter = 0;
+		int i, n;
 
-			while (*(s + i))
-						i++;
-				i = i - 1;
-					while (i >= 0)
-							{
-										_putchar(*(s + i));
-												i--;
-													}
-						_putchar('\n');
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			fcounter++;
+		}
+
+		for (n = (fcounter - 1); n >= 0; n--)
+		{											_putchar(s[n]);
+		}
+		_putchar('\n');
 }
