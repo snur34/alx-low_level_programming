0f4ccf3a044b078020b79e7904889514e2cbde6b
snur34 <a607514@gmail.com>
2022-09-20 13:57:49 -0700

    t7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *puts_half - Prints out the first half of a string
+ *@str: input string to print.
+ */
+void puts_half(char *str)
+{
+	int j = 0;
+	int k;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	if (j % 2 == 1)
+	{
+		k = (j - i)/ 2;
+		k += 1;
+	}
+	else
+	{
+		k = j / 2;
+	}
+
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
+	}
+
+	_putchar('\n');
+}
